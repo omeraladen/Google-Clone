6bcf5dbb96137a0df61274b3058aee2445f67c8a
omeraladen <killerpop1738@gmail.com>
2022-05-18 04:25:28 +0200

    fixing the search navigation using useNavegation() method

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 import MicIcon from '@mui/icons-material/Mic';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 
 function Search () {
+    const [input , setInput] = useState(""); 
+    const navigate = useNavigate();
 
-    cont search = e => { e.preventDefault(); }
+    const search = (e) => { 
+        e.preventDefault();  //prevent a browser reload/refresh
+
+        navigate('/search');
+
+    }
 
     return(
-    <div className='search'>
+    <form className='search'>
+
             <div className='search__input'>
                 <SearchIcon className='search__inputIcon'/>
-                <input/>
+                <input value={input} onChange={e => setInput(e.target.value)}/>
                 <MicIcon/>
             </div>
 
             <div className='search__buttons'>
-                <Button onClick={search} variant="outlined">Google Search </Button>
+                <Button type='submit' onClick={search}      variant="outlined">Google Search </Button>
                 <Button variant="outlined">I'm Feeling Lucky </Button>
             </div>
-    </div>
+    
+    </form>
 
     )
     
