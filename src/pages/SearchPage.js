d85dc0520beddc62581a031f3db226a924624432
omeraladen <killerpop1738@gmail.com>
2022-07-18 12:18:15 +0200

    styling the results Page with options links

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,7 +5,7 @@ import useGoogleSearch from '../component/useGoogleSearch';
 import Response from '../response'
 import { Link } from 'react-router-dom';
 import Search from '../component/Search';
-
+//icons =>  https://mui.com/material-ui/material-icons/?query=ma&selected=Room
 import SearchIcon from '@mui/icons-material/Search';
 import DescriptionIcon from '@mui/icons-material/Description';
 import ImageIcon from '@mui/icons-material/Image';
@@ -50,8 +50,9 @@ return(
 
             <div className="searchPage_optionsLeft"> {/*--------searchPage left Side--------------*/}
                 <div className="searchPage_option">
-                    <SearchIcon/>
+                    <svg focusable="false" viewBox="0 0 24 24"><path fill="#34a853" d="M10 2v2a6 6 0 0 1 6 6h2a8 8 0 0 0-8-8"></path><path fill="#ea4335" d="M10 4V2a8 8 0 0 0-8 8h2c0-3.3 2.7-6 6-6"></path><path fill="#fbbc04" d="M4 10H2a8 8 0 0 0 8 8v-2c-3.3 0-6-2.69-6-6"></path><path fill="#4285f4" d="M22 20.59l-5.69-5.69A7.96 7.96 0 0 0 18 10h-2a6 6 0 0 1-6 6v2c1.85 0 3.52-.64 4.88-1.68l5.69 5.69L22 20.59"></path></svg>
                     <Link to="/all">All</Link>
+                    <div className='bar'></div>
                 </div>
                 <div className="searchPage_option">
                     <DescriptionIcon/>
@@ -84,10 +85,33 @@ return(
                  <Link to="/tools">Tools</Link>
                  </div>
             </div>
-
         </div>
         </div>
     </div>
+                                                         {/*--------Results ► Link , Title , Paragraph --------------*/}
+             {true && (
+                <div
+                className='searchPage__results'>
+                    <p
+                    className='searchPage__resultCount'>
+                        About {data?.searchInformation.formattedTotalResults} results
+                        ({data?.searchInformation.formattedSearchTime} seconds)
+                         {term}
+                    </p>  
+
+                    {data?.items.map(item => (
+                        <div className='searchPage__result'>
+                            <a className='searchPage__resultLink' href={item.link}> {item.displayLink}... <MoreVertIcon/> </a> 
+                             
+                            <a className='searchPage__resultTitle' href={item.link}> <h2>{item.title}</h2>  </a>
+
+                            <p className='searchPage__resultSnippet'> {item.snippet} </p> 
+                        </div>
+                    ))}
+                </div>
+             )
+
+             }
     </div>
     )
 }
