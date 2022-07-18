2b066c3be765a9b9d78dfb99ea29fb39ac2a460b
omeraladen <killerpop1738@gmail.com>
2022-07-18 09:00:31 +0200

    bulding the search result and search options(all,news,images,..)

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,6 +3,15 @@ import './SearchPage.css';
 import {useStateValue} from "../StateProvider/StateProvider";
 import useGoogleSearch from '../component/useGoogleSearch';
 import Response from '../response'
+import { Link } from 'react-router-dom';
+import Search from '../component/Search';
+
+import SearchIcon from '@mui/icons-material/Search';
+import DescriptionIcon from '@mui/icons-material/Description';
+import ImageIcon from '@mui/icons-material/Image';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import RoomIcon from '@mui/icons-material/Room';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 // get the custom google key api from 'https://developers.google.com'
 // to enable the search the entire page go to 'cse.google.com' and get context id
@@ -25,16 +34,61 @@ const data = Response;
 
 console.log(data);
 return(
-        <div className='search-page'>
-             <div className='search-page_header'>
-                <h1>{term}</h1>
-               
+    <div className="searchPage">
+    {/*<h1>This is search page WoW!!!!!</h1>*/}
+    <div className="searchPage_header">
+        <Link to="/">
+            <img
+                className="searchPage_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"
+                alt=""/>
+        </Link>
+        <div className="searchPage_headerBody">
+            <Search  hideButtons/>
+          <div className="searchPage_options">
+
+
+            <div className="searchPage_optionsLeft"> {/*--------searchPage left Side--------------*/}
+                <div className="searchPage_option">
+                    <SearchIcon/>
+                    <Link to="/all">All</Link>
+                </div>
+                <div className="searchPage_option">
+                    <DescriptionIcon/>
+                    <Link to="/news">News</Link>
+                </div>
+                <div className="searchPage_option">
+                    <ImageIcon/>
+                    <Link to="/images">Images</Link>
+                </div>
+                <div className="searchPage_option">
+                    <LocalOfferIcon/>
+                    <Link to="/shopping">Shopping</Link>
+                </div>
+                <div className="searchPage_option">
+                    <RoomIcon/>
+                    <Link to="/maps">Maps</Link>
+                </div>
+                <div className="searchPage_option">
+                    <MoreVertIcon/>
+                    <Link to="/more">More</Link>
+                </div>
             </div>
-            
-            <div className='search-page_results'>
-                
+        
+
+            <div className="searchPage_optionsRight"> {/*--------searchPage Right Side--------------*/}
+              <div className="searchPage_option">
+                   <Link to="/settings">Settings</Link>
+                    </div>
+                 <div className="searchPage_option">
+                 <Link to="/tools">Tools</Link>
+                 </div>
             </div>
+
         </div>
+        </div>
+    </div>
+    </div>
     )
 }
 
